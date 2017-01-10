8d78e85ae68c2c0278d12fda8c7138ab2e79f2c9
Gregory Todd Williams <greg@evilfunhouse.com>
2017-01-10 13:02:57 -0800

    Updated to use github-hosted serd wrapper.

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftSerd",
-    dependencies: [.Package(url: "../serd", versions: Version(0,0,1)..<Version(1,0,0))]
+    name: "SerdParser",
+    dependencies: [
+		.Package(url: "https://github.com/kasei/swift-serd.git", majorVersion: 0)
+    ]
 )
